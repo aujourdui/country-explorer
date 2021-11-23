9d3b10eca60a313945f7a63c262ba35f34f2e0df
kyosuke ito <kyosuke.ito@gmail.com>
2021-11-22 16:04:21 -0800

    change country name by injecting way

--- index.js
+++ index.js
@@ -2,3 +2,18 @@
                                   Global TODO:
 - 
 ---------------------------------------+--------------------------------------*/
+
+const addCountryName = () => {
+  for (let i = 0; i < 6; i++) {
+    const countryName = document.getElementsByClassName("country-name")[i];
+    countryName.innerHTML = "hello world";
+  }
+};
+addCountryName();
+
+const zoomIn = () => {
+  // const countryName = document.getElementsByClassName("country-name")[0];
+  // countryName.innerHTML = "hello world";
+};
+
+// zoomIn();
