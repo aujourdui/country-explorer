bab815da95ddb97d32c2ac6a147d2274e54c5579
kyosuke ito <kyosuke.ito@gmail.com>
2021-11-23 18:15:42 -0800

    add basic eventlistner for zoom in

--- index.js
+++ index.js
@@ -6,14 +6,16 @@
 const addCountryName = () => {
   for (let i = 0; i < 8; i++) {
     const countryName = document.getElementsByClassName("country-name")[i];
-    countryName.innerHTML = "hello world";
+    countryName.innerHTML = "German";
   }
 };
 addCountryName();
 
 const zoomIn = () => {
-  // const countryName = document.getElementsByClassName("country-name")[0];
-  // countryName.innerHTML = "hello world";
+  const countryCard = document.getElementsByClassName("country-card")[0];
+  countryCard.addEventListener("click", () => {
+    // e.preventDefault();
+    alert("hello");
+  });
 };
-
-// zoomIn();
+zoomIn();
